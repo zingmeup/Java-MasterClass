f971ab2e20ced900a02f704a035c75180e3ee95a
Deepak Yadav <34516539+deepakyadav23@users.noreply.github.com>
2019-01-28 23:07:32 +0530

    Simple Student Class

---
+++ Launcher.java
@@ -0,0 +1,50 @@
+package packageabc;
+
+class Student{
+    private String name, uid;
+    byte age;
+
+    public Student(String name, String uid, byte age) {
+        this.name = name;
+        this.uid = uid;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return this.name+" "+this.uid+" "+this.age;
+    }
+}
+
+public class Launcher {
+    public static void main(String[] args) {
+        Student s1=new Student("Ashik", "17bcs1520", (byte)20);
+        System.out.println(s1);
+
+    }
+
+}
